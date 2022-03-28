e6873ec6e5f97aff1be2e7a11cd29992b7dd1cfb
varapsd <varaprasad3638@gmail.com>
2022-03-28 16:01:20 +0530

    interface

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import DataTableDynamicDemo from './components/order';
 import { useNavigate, BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Button } from "primereact/button";
 import { TabMenu } from 'primereact/tabmenu';
+import EnquiryData from './components/Enquiry';
 
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
@@ -16,7 +17,8 @@ import "primeicons/primeicons.css";                                //icons
 function App() {
   const items = [
     {label: 'Home', icon: 'pi pi-fw pi-home'},
-    {label: 'Orders', icon: 'pi pi-fw pi-calendar'}
+    {label: 'Orders', icon: 'pi pi-fw pi-calendar'},
+    {label: 'Enquiries', icon: 'pi pi-fw pi-question-circle'}
 ];
 const [activeIndex, setActiveIndex] = useState(0);
 let navigate = useNavigate();
@@ -25,6 +27,9 @@ const routePages = (index)=>{
   setActiveIndex(index); 
   if(items[index].label == "Orders"){
     navigate("/orders")
+  }
+  else if(items[index].label == "Enquiries"){
+    navigate("/enquiries")
   }
   else{
      navigate("/")
@@ -46,6 +51,7 @@ const routePages = (index)=>{
         <Route path="/" element={<DataTableEditDemo />}></Route>
         <Route path="/Home" element={<DataTableEditDemo />}></Route>
         <Route path="/Orders" element={<DataTableDynamicDemo />}></Route>
+        <Route path="/Enquiries" element={<EnquiryData />}></Route>
       </Routes>
       </div>
     </div>
---
+++ src/components/Enquiry.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { ProgressSpinner } from 'primereact/progressspinner';
+import { Dialog } from 'primereact/dialog';
+import axios from 'axios';
+import "./order.css"
+
+const EnquiryData = () => {
+    const [products, setProducts] = useState([]);
+    const [ loading, setLoading] = useState(false);
+    const columns = [
+        {field: 'enquiryId', header: 'Enquiry Id'},
+        {field: 'productId', header: 'Product Id'},
+        {field: 'name', header: 'Name'},
+        {field: 'phone', header: 'Phone'}
+    ];
+
+    const url = "https://demo-gupshup-flow.herokuapp.com/"
+    useEffect(()=>{
+        setLoading(true);
+        axios.get(url+"getAllEnquiries")
+            .then(function (response) {
+                setProducts(response.data);
+                setLoading(false)
+            })
+            .catch(function (error) {
+                console.log(error);
+                setLoading(false);
+            })
+    },[])
+
+
+    const dynamicColumns = columns.map((col,i) => {
+        return <Column key={col.field} field={col.field} header={col.header} />;
+    });
+
+    const modalLoad = ()=>{
+        return(
+        <Dialog visible={loading}>
+            <ProgressSpinner />
+        </Dialog>
+        )
+    }
+
+    return (
+        <div>
+             <h2>Enquiries List</h2>
+            <div className="card">
+                {modalLoad()}
+                <DataTable value={products} responsiveLayout="scroll">
+                    {dynamicColumns}
+                </DataTable>
+            </div>
+        </div>
+    );
+}
+
+export default EnquiryData;
